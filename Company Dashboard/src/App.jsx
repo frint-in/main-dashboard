3153bf1848ca03a8befbb10c843bfd5c55c22979
Ritav Kashyap <ritav20-237@aec.ac.in>
2024-03-07 19:33:08 +0530

    update

--- Company Dashboard/src/App.jsx
+++ Company Dashboard/src/App.jsx
@@ -1,16 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Admin from "./layouts/admin";
-import Auth from "./layouts/auth";
+// import Auth from "./layouts/auth";
 import PendingDashboard from "./views/admin/Orders_sidebar/Pending_sidebar";
 import Profile from "./views/admin/profile";
-
+import Auth from "./views/auth/Auth";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="auth/*" element={<Auth />} />
+        {/* <Route path="auth/*" element={<Auth />} /> */}
+        <Route path="/auth" element={<Auth />} />
         <Route path="admin/*" element={<Admin />} />
         {/* <Route path="admin/applicants" element={<PendingDashboard/>} />
         <Route path="admin/profile" element={<Profile/>} /> */}
