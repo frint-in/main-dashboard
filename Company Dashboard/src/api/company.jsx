d20abaa87df8b588f4734adbb80c4d671df6eb7b
Tirthankar03 <tirthankar.nath2@gmail.com>
2024-03-07 18:43:39 +0530

    wow

--- Company Dashboard/src/api/company.jsx
+++ Company Dashboard/src/api/company.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const AXIOS_BASE_URL = 'http://localhost:8000'
+const AXIOS_BASE_URL = import.meta.env.VITE_REACT_API_BASE_ROUTE
 
 export async function addInternship() {
     const response = await axios.post(`${AXIOS_BASE_URL}/api/internship/addinternship`);
@@ -12,7 +12,7 @@ export async function addInternship() {
 export async function getAllInterships() {
   try {
     console.log("hi1");
-    const response = await axios.get(`http://localhost:8000/api/internship/all`);
+    const response = await axios.get(`${AXIOS_BASE_URL}/api/internship/all`);
     console.log("hi1");
 
     const data = response.data;
@@ -26,32 +26,4 @@ export async function getAllInterships() {
 
 }
 
- 
-  export async function createPost(newPost) {
-    const response = await fetch(`http://localhost:3000/posts`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(newPost)
-    });
-    return response.json()
-  }
   
-  export async function updatePost(updatedPost) {
-    const response = await fetch(`http://localhost:3000/posts/${updatedPost.id}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(updatedPost)
-    });
-    return response.json()
-  }
-  
-  export async function deletePost(id) {
-    const response = await fetch(`http://localhost:3000/posts/${id}`, {
-      method: "DELETE",
-    });
-    return response.json()
-  }
