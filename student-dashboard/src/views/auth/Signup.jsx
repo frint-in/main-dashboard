4cb1b26ec4d66858a2278ab12a8ac381d0158694
Ritav Kashyap <ritav20-237@aec.ac.in>
2024-03-21 10:02:04 +0530

    just changed

--- student-dashboard/src/views/auth/Signup.jsx
+++ student-dashboard/src/views/auth/Signup.jsx
@@ -3,46 +3,40 @@ import { useState } from "react";
 import axios from "axios";
 import { useNavigate, Link } from "react-router-dom";
 
+export default function Signup({ setIsAdminAuthenticated }) {
+  const navigate = useNavigate();
 
-export default function Signup({setIsAdminAuthenticated}) {
-
-  const navigate = useNavigate()
-
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [phno, setphno] = useState('');
-  const [uname, setUname] = useState('');
-
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phno, setphno] = useState("");
+  const [uname, setUname] = useState("");
 
   const handleUser = async (e) => {
-    
     e.preventDefault();
 
     try {
-      const res = await axios.post('https://api.frint.in/api/auth/signup', { email, password, uname, phno },
-      { withCredentials: true }
-
+      const res = await axios.post(
+        "https://api.frint.in/api/auth/signup",
+        { email, password, uname, phno },
+        { withCredentials: true }
       );
       if (res.data) {
         // console.log(res.data);
-        navigate('/auth');
+        navigate("/auth");
         // setIsAdminAuthenticated(true)
         // sessionStorage.setItem('token', res.data.token)
-        alert('Sign in successfull')
-
+        alert("Sign in successfull");
       } else {
-        alert('Invalid Credentials')
+        alert("Invalid Credentials");
         // console.log('invalid credentials');
       }
     } catch (error) {
       // console.log(error);
-      
     }
-    
-  }
+  };
 
   return (
-    <div className="mt-16 mb-16 flex h-full w-full items-center justify-center px-2 md:mx-0 md:px-0 lg:mb-10 lg:items-center lg:justify-center" >
+    <div className="mt-16 mb-16 flex h-full w-full items-center justify-center px-2 md:mx-0 md:px-0 lg:mb-10 lg:items-center lg:justify-center">
       {/* Sign in section */}
       <div className="mt-[10vh] w-full max-w-full flex-col items-center md:pl-4 lg:pl-0 xl:max-w-[420px]">
         <h4 className="mb-2.5 text-4xl font-bold text-navy-700 dark:text-white text-center">
@@ -62,55 +56,55 @@ export default function Signup({setIsAdminAuthenticated}) {
           <div className="h-px w-full bg-gray-200 dark:bg-navy-700" />
         </div> */}
         {/* Email */}
-        <form onSubmit={handleUser} >
-        <InputField
-          // variant="auth"
-          extra="mb-3"
-          label="Email*"
-          placeholder="mail@simmmple.com"
-          id="email"
-          type="text"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
+        <form onSubmit={handleUser}>
+          <InputField
+            // variant="auth"
+            extra="mb-3"
+            label="Email*"
+            placeholder="mail@simmmple.com"
+            id="email"
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
-        {/* Password */}
-        <InputField
-          // variant="auth"
-          extra="mb-3"
-          label="Password*"
-          placeholder="********"
-          id="password"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)} 
-        />
+          {/* Password */}
+          <InputField
+            // variant="auth"
+            extra="mb-3"
+            label="Password*"
+            placeholder="********"
+            id="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-<InputField
-          // variant="auth"
-          extra="mb-3"
-          label="Name*"
-          placeholder=""
-          id="uname"
-          type="text"
-          value={uname}
-          onChange={(e) => setUname(e.target.value)}
-        />
+          <InputField
+            // variant="auth"
+            extra="mb-3"
+            label="Name*"
+            placeholder="Full Name"
+            id="uname"
+            type="text"
+            value={uname}
+            onChange={(e) => setUname(e.target.value)}
+          />
 
-        {/* Password */}
-        <InputField
-          // variant="auth"
-          extra="mb-3"
-          label="Phone number*"
-          placeholder="********"
-          id="phno"
-          type="text"
-          value={phno}
-          onChange={(e) => setphno(e.target.value)} 
-        />
-        
-        {/* Checkbox */}
-        {/* <div className="mb-4 flex items-center justify-between px-2">
+          {/* Password */}
+          <InputField
+            // variant="auth"
+            extra="mb-3"
+            label="Phone number*"
+            placeholder="+91 ..."
+            id="phno"
+            type="text"
+            value={phno}
+            onChange={(e) => setphno(e.target.value)}
+          />
+
+          {/* Checkbox */}
+          {/* <div className="mb-4 flex items-center justify-between px-2">
           <div className="flex items-center">
             <Checkbox />
             <p className="ml-2 text-sm font-medium text-navy-700 dark:text-white">
@@ -124,14 +118,19 @@ export default function Signup({setIsAdminAuthenticated}) {
             Forgot Password?
           </a>
         </div> */}
-        <p className="mb-9 ml-1 text-base text-gray-600 text-center">
-          Already have an account? <Link to="/" className="text-brand-500"> Log in </Link> 
-        </p>
-        <button className="linear mt-2 w-full rounded-xl bg-brand-500 py-[12px] text-base font-medium text-white transition duration-200 hover:bg-brand-600 active:bg-brand-700 dark:bg-brand-400 dark:text-white dark:hover:bg-brand-300 dark:active:bg-brand-200"
-         type="submit"
-         >
-          Sign In
-        </button>
+          <p className="mb-9 ml-1 text-base text-gray-600 text-center">
+            Already have an account?{" "}
+            <Link to="/" className="text-brand-500">
+              {" "}
+              Log in{" "}
+            </Link>
+          </p>
+          <button
+            className="linear mt-2 w-full rounded-xl bg-brand-500 py-[12px] text-base font-medium text-white transition duration-200 hover:bg-brand-600 active:bg-brand-700 dark:bg-brand-400 dark:text-white dark:hover:bg-brand-300 dark:active:bg-brand-200"
+            type="submit"
+          >
+            Register
+          </button>
         </form>
         {/* <div className="mt-4">
           <span className=" text-sm font-medium text-navy-700 dark:text-gray-600">
