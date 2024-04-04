7343ae712adafa7a453b124fe610bff606ce6a2e
Ritav Kashyap <ritav20-237@aec.ac.in>
2024-04-04 17:05:49 +0530

    idk

--- student-dashboard/src/views/admin/singleInternship/components/General.jsx
+++ student-dashboard/src/views/admin/singleInternship/components/General.jsx
@@ -27,7 +27,7 @@ const General = () => {
     <Card extra={"w-full h-full p-3"}>
       {/* Header */}
       <div className="mt-2 mb-8 w-full">
-        <img src={internship?.imgurl} alt="" className="w-auto h-20 p-2" />
+        <img src={internship?.imgurl} alt="" className="w-1/2 p-2" />
         <h4 className="px-2 text-xl font-bold text-navy-700 dark:text-white">
         {internship?.name || 'Company Name' }
         </h4>
